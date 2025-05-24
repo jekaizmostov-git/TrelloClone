0528849c4a782ddddd1166c8c6086edbb5508139
jekaizmostov <jekaizmostov@gmail.com>
2025-05-25 00:28:59 +0300

    min implementation dragAndDrop for dinamyc created cards and boards

--- script/main.js
+++ script/main.js
@@ -1,5 +1,5 @@
 const trello = document.querySelector('.trello')
-const board = document.querySelectorAll('.board')
+//const boards = document.querySelectorAll('.board')
 const addBoardBtn = document.querySelector('.add_board_btn')
 
 const addListItemBtn = document.querySelector('.add_listitem_btn')
@@ -10,7 +10,6 @@ const formAddListItemButton = form.querySelector('.add_list_item_btn')
 const formTextArea = form.querySelector('.form-textarea')
 
 const lists = document.querySelectorAll('.list')
-// const titles = document.querySelectorAll('.title')
 
 
 
@@ -48,17 +47,19 @@ formAddListItemButton.addEventListener('click', event => {
 	lists[0].appendChild(listItem)
 
 	formTextArea.value = ''
-	formAddListItemButton.style.visibility = 'hidden'
+	form.style.display = 'none'
+	addListItemBtn.style.display = 'block'
+
 })
 
 
-// //delete card by right click 
-// board.addEventListener('contextmenu', event => {
-// 	event.preventDefault()
-// 	if (event.target.closest('.list-item')){
-// 		list.removeChild(event.target)
-// 	}
-// })
+//delete card by right click 
+trello.addEventListener('contextmenu', event => {
+	event.preventDefault()
+	if (event.target.closest('.list-item')){
+		event.target.remove()
+	}
+})
 
 //create new board on add_board_btn click
 function createBoard() {
@@ -77,12 +78,69 @@ function createBoard() {
 
 
 	trello.appendChild(board)
+	console.log(board)
 }
 addBoardBtn.addEventListener('click', createBoard)
 
 //clear board title on click
 trello.addEventListener('click', event => {
+
 	if (event.target.closest('.title')){
-		event.target.textContent = ''
+		let title = event.target
+		title.textContent = ''
+	}
+})
+
+
+//min implementation to drag and drop for dinamyc created cards and boards
+trello.addEventListener('dragstart', handleDragStart)
+trello.addEventListener('dragover', handleDragOver)
+trello.addEventListener('drop', handleDrop)
+
+let dragItem = null
+function findValidDropTarget(element){
+	while(element){
+		if (element.classList.contains('board')){
+			return element
+		}
+		if (element.classList.contains('trello')){
+			return null
+		}
+		element = element.parentNode
+	}
+	return null
+}
+
+function handleDragStart(e){
+	if (e.target.classList.contains('list-item')){
+		dragItem = e.target
+	}
+}
+
+function handleDragOver(e) {
+	const dropZone = findValidDropTarget(e.target)
+	if (dropZone){
+		e.preventDefault()
+	}
+}
+
+function handleDrop(e) {
+	if (!dragItem) return;
+
+	const dropZone = findValidDropTarget(e.target)
+	if (dropZone && dragItem){
+		e.preventDefault()
+		if (dropZone !== dragItem.parentNode){
+			dropZone.querySelector('.list').appendChild(dragItem)
+		}
+	}
+
+	
+}
+
+document.addEventListener('dragend', event => {
+	if (dragItem){
+		dragItem = null
+		console.log('dragEnd')
 	}
 })
